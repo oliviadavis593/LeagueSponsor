1da82f1bdc03432e752cde4d185920e1c2a7d7c0
Olivia Davis <olivia.davis593@gmail.com>
2020-07-03 21:52:44 -0500

    league form implemented

--- src/components/LeagueForm.js
+++ src/components/LeagueForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from '../imgs/leaguelogo.png';
+import '../styles/LeagueForm.css'
 
 
 class LeagueForm extends Component {
@@ -13,9 +14,53 @@ class LeagueForm extends Component {
                     </NavLink>
                 </div>
                 <main>
-                    <form>
+                    <form
+                    className='ls-leagueform'
+                    >
+                        <fieldset>
+                            <legend>
+                                <h2>League Form</h2>
+                                <p>
+                                    Add a league to our platform so sponsorships can see you!
+                                </p>
+                            </legend>
 
-                    </form>
+                            <label htmlFor='league_name'>League Name:</label>
+                            <input 
+                            id='league_name'
+                            type='text'
+                            name='league_name'
+                            required
+                            />
+
+                            <label htmlFor='Location'>Location:</label>
+                            <input 
+                            id='location'
+                            type='text'
+                            name='location'
+                            required
+                            />
+
+                            <label htmlFor='website'>Website:</label>
+                            <input 
+                            id='website'
+                            type='text'
+                            name='website'
+                            />
+
+                            <label htmlFor='price'>Price:</label>
+                            <input 
+                            id='price'
+                            type='text'
+                            name='price'
+                            />
+                        </fieldset>
+                        <button
+                        className='ls-form__button'
+                        >
+                            Submit
+                        </button>
+                        </form> 
                 </main>
             </div>
         )
