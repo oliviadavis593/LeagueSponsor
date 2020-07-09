cf187018fad64fa73f121b291f8d0edbca03027d
Olivia Davis <olivia.davis593@gmail.com>
2020-07-09 14:44:59 -0500

    unresolved

--- src/components/SponsorPage.js
+++ src/components/SponsorPage.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import config from '../config';
 import Logo from '../imgs/leaguelogo.png';
 import '../styles/SponsorPage.css';
 
 class SponsorPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            parameters: {},
+            leagues: [],
+            error: null,
+        }
+    }
+
+    hasRequiredParams = parameters => {
+        const { latitude, longitude, radius, budget } = this.state.parameters;
+        if (latitude && longitude && radius && budget) {
+            return true; 
+        }
+        return false; 
+    }
+
+    handleSumbit = event => {
+        event.preventDefault();
+        const parameters = {};
+
+        Object.values(event.target.elements).forEach(input => {
+            if (input.tagNaem === 'INPUT') {
+                parameters[input.name] = input.value; 
+            }
+        });
+
+        this.setState({...this.state, parameters });
+    }
+
+    componentDidUpdate(_, prevState) {
+        if (this.hasRequiredParams()) {
+            if (
+                prevState.parameters.latitude !== this.state.parameters.latitude ||
+                prevState.parameters.longitude !== this.state.parameters.longitude ||
+                prevState.parameters.radius !== this.state.parameters.radius ||
+                prevState.parameters.budget !== this.state.parameters.budget
+            ) {
+                const { latitude, longitude, radius, budget } = this.state.parameters; 
+                fetch(`${config.API_ENDPOINT}/api/leagues?latitude=${latitude}&longitude=${longitude}&radius=${radius}&budget=${budget}`)
+                    .then(res => {
+                        if (res.ok) {
+                            return res.json();
+                        }
+                    })
+                    .then(leagues => {
+                        this.setState({ leagues, error: null })
+                    })
+                    .catch(error => {
+                        this.setState({
+                            ...this.state, 
+                            error: 'Failed to fetch the leagues'
+                        });
+                    });
+            }
+        } 
+    }
+
     render() {
         return(
             <div>
@@ -13,6 +72,7 @@ class SponsorPage extends Component {
                 <main>
                     <form
                     className='ls-sponsor__form'
+                    onSubmit={this.handleSumbit}
                     >
                         <fieldset>
                             <legend>
@@ -21,7 +81,7 @@ class SponsorPage extends Component {
                             <label htmlFor='budget'>Total Budget:</label>
                             <input 
                             id='budget'
-                            type='text'
+                            type='number'
                             name='budget'
                             />
 
@@ -41,7 +101,6 @@ class SponsorPage extends Component {
 
                             <label htmlFor='radius'>Radius:</label>
                             <select name='radius' id='radius'>
-                                <option value='Any'>Any</option>
                                 <option value='5'>5mi</option>
                             </select>
                         </fieldset>
@@ -52,6 +111,7 @@ class SponsorPage extends Component {
                                 Find
                             </button>
                         </div>
+                        {this.state.error && <p>{this.state.error}</p>}
                     </form>
                 </main>
             </div>
